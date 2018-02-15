2dcc05c235192db3de065448a767ad46b030f9b3
dkrychylskyy <d.krychylskyy@gmail.com>
2018-02-15 16:47:44 +0100

    Creation les objets principaux. Add fonctions á GameBoard. Add tests pour premier ligne et premier col

--- Snake/src/com/dk/snake/Main.java
+++ Snake/src/com/dk/snake/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		GameBoard gb = new GameBoard(5, 5);
+		System.out.println(gb.toString());
 	}
 
 }
